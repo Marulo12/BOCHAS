5a8d5678b612a68aec266ab02b08b4a695bfc39c
Marulo12 <39140499+Marulo12@users.noreply.github.com>
2018-05-25 18:36:04 -0300

    Modulo Cambio de Contraseña

---
+++ BOCHAS/wwwroot/js/Usuarios.js
@@ -0,0 +1,72 @@
+﻿$(document).ready(function () {
+
+    $("#BtnCambiaContra").click(function () {
+
+        VerificarContraseña();
+
+    });
+
+});
+
+function CambiaContraseña(contraactual) {
+    
+    var contranueva = $("#PassNuevo").val();
+    var contraconfirma = $("#PassConfirma").val();
+
+    if (ValidarContraseñas(contranueva, contraconfirma)) {
+        $.ajax({
+            type: "POST",
+            url: "/Usuarios/CambiarContraseña",
+            data: { contraactual, contranueva },
+            success: function (response) {
+                if (response == "OK") {
+
+                    alertify.success("Clave Modificada con exito");
+                    $("#PassActual").val("");
+                    $("#PassNuevo").val("");
+                    $("#PassConfirma").val("");
+                }
+                else {
+                    alertify.alert("Se produjo un error en la operacion");
+                 
+                }
+
+            }
+        });
+
+    }
+
+
+}
+
+function  VerificarContraseña() {
+    
+    var contraactual = $("#PassActual").val();
+    $.ajax({
+        type: "GET",
+        url: "/Usuarios/VerificarContraseña",
+        data: { contraactual },
+        success: function (response) {
+            if (response === "OK") {
+                CambiaContraseña(contraactual);
+            }
+            else {
+                alertify.error("la contraseña original no es valida");
+   
+            }
+        }
+    });
+
+   
+}
+
+function ValidarContraseñas(contranueva, contraconfirma) {
+    if (contranueva === contraconfirma) {
+        return true;
+
+    }
+    else {
+        alertify.error("Las contraseñas no coinciden");
+        return false;
+    }
+}
