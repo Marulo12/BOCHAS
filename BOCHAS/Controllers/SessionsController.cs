051636fdc63fea27ae478051745132dc0a6900d1
Marulo12 <39140499+Marulo12@users.noreply.github.com>
2018-05-25 11:50:16 -0300

    Correcciones CSS

---
+++ BOCHAS/Controllers/SessionsController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BOCHAS.Models;
+using Microsoft.AspNetCore.Authorization;
+
+namespace BOCHAS.Controllers
+{[Authorize]
+    public class SessionsController : Controller
+    {
+        private readonly BOCHASContext _context;
+
+        public SessionsController(BOCHASContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Sessions
+        public  IActionResult Index()
+        {
+           
+            return View();
+        }
+
+        
+        public async Task<IActionResult> MostrarSessiones(string fechadesde, string fechahasta)
+        {
+            if (fechadesde == null && fechahasta == null)
+            {
+                return NotFound();
+            }
+
+            var session = await _context.Session.Include(s => s.IdUsuarioNavigation).Where(s =>s.FechaInicio >= Convert.ToDateTime(fechadesde) && s.FechaInicio <= Convert.ToDateTime(fechahasta)).ToListAsync();
+            if (session == null)
+            {
+                return NotFound();
+            }
+
+            return PartialView(session);
+        }
+
+        
+       
+    }
+}
