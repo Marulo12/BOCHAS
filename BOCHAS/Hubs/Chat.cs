3f359a55a4098c4bd036f0d9173c0ef962e80d2e
Marulo12 <39140499+Marulo12@users.noreply.github.com>
2018-06-17 09:42:12 -0300

    Carpeta Noticias

--- BOCHAS/Hubs/Chat.cs
+++ BOCHAS/Hubs/Chat.cs
@@ -4,14 +4,17 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BOCHAS.Models;
 
 namespace BOCHAS.Hubs
 {[HubName("chat")]
     public class Chat: Hub
     {
-        public void join()
+        public void join(string user)
         {
-            Clients.All.join($"{Context.ConnectionId } se conecto");
+            BOCHASContext bd = new BOCHASContext();
+            var users = (from u in bd.Usuario join s in bd.Session on u.Id equals s.IdUsuario join p in bd.Persona on u.Id equals p.IdUsuario where p.Tipo=="JUGADOR" &&  s.FechaInicio==DateTime.Now.Date && s.FechaFin == null select new { us=u.Nombre }).ToList().Distinct();
+            Clients.All.join(users);
         }
     }
 }
