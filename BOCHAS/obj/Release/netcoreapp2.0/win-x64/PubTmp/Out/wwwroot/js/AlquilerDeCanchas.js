604cbaa04202038f2a3e82f67b1731eadd4b42eb
mario boscatto <mariolucbos@hotmail.com>
2018-08-07 10:18:53 -0300

    AutoComplete

--- BOCHAS/obj/Release/netcoreapp2.0/win-x64/PubTmp/Out/wwwroot/js/AlquilerDeCanchas.js
+++ BOCHAS/obj/Release/netcoreapp2.0/win-x64/PubTmp/Out/wwwroot/js/AlquilerDeCanchas.js
@@ -1,5 +1,6 @@
 ﻿
 $(document).ready(function () {
+    AutoCompletarCombo();
     $('[data-toggle="tooltip"]').tooltip(); 
     $("#BtnValida").click(function () {
 
@@ -311,16 +312,16 @@ function ComprobarCamposDates() {
 
 function ConsultaParticular() {
     var nombreP = $("#nombreP").val();
-    
-    if (nombreP == "" ) {
-        alertify.error("Complete el campo de consulta");
+    var apellidoP = $("#apellidoP").val();
+    if (nombreP === "" || apellidoP === "" ) {
+        alertify.error("Complete los campos de consulta");
        
     } else {
         $("#TablaConPar").empty();
         $("#ImgLoad").css("display", "inline-block");
         $.ajax({
             type: "GET",
-            data: {nombreP},
+            data: {nombreP,apellidoP},
             url: "/AlquilerCanchas/ConsultaReservaParticular",
             success: function (response) {
                 $("#ImgLoad").css("display", "none");
@@ -334,9 +335,19 @@ function ConsultaParticular() {
                     buttons: [
 
                         {
+                            extend: 'excel',
+                            text: 'Excel',
+                            title: 'BOCHAS PADEL - Reservas'
+                        },
+                        {
+                            extend: 'pdfHtml5',
+                            text: 'PDF',
+                            title: 'BOCHAS PADEL - Reservas'
+                        },
+                        {
                             extend: 'print',
                             text: 'Imprimir',
-                            title: 'BOCHAS PADEL - Mis Reservas'
+                            title: 'BOCHAS PADEL - Reservas'
 
                         }
                     ],
@@ -408,5 +419,145 @@ function VerDetalleReserva(numero) {
         }
 
     });
+
+}
+
+function AutoCompletarCombo() {
+
+    $.widget("custom.combobox", {
+        _create: function () {
+            this.wrapper = $("<span>")
+                .addClass("custom-combobox")
+                .insertAfter(this.element);
+
+            this.element.hide();
+            this._createAutocomplete();
+            this._createShowAllButton();
+        },
+
+        _createAutocomplete: function () {
+            var selected = this.element.children(":selected"),
+                value = selected.val() ? selected.text() : "";
+
+            this.input = $("<input>")
+                .appendTo(this.wrapper)
+                .val(value)
+                .attr("title", "")
+                .addClass("custom-combobox-input ui-widget ui-widget-content ui-state-default ui-corner-left")
+                .autocomplete({
+                    delay: 0,
+                    minLength: 0,
+                    source: $.proxy(this, "_source")
+                })
+                .tooltip({
+                    classes: {
+                        "ui-tooltip": "ui-state-highlight"
+                    }
+                });
+
+            this._on(this.input, {
+                autocompleteselect: function (event, ui) {
+                    ui.item.option.selected = true;
+                    this._trigger("select", event, {
+                        item: ui.item.option
+                    });
+                },
+
+                autocompletechange: "_removeIfInvalid"
+            });
+        },
+
+        _createShowAllButton: function () {
+            var input = this.input,
+                wasOpen = false;
+
+            $("<a>")
+                .attr("tabIndex", -1)
+                .attr("title", "Mostrar todos los Jugadores")
+                .tooltip()
+                .appendTo(this.wrapper)
+                .button({
+                    icons: {
+                        primary: "ui-icon-triangle-1-s"
+                    },
+                    text: false
+                })
+                .removeClass("ui-corner-all")
+                .addClass("custom-combobox-toggle ui-corner-right")
+                .on("mousedown", function () {
+                    wasOpen = input.autocomplete("widget").is(":visible");
+                })
+                .on("click", function () {
+                    input.trigger("focus");
+
+                    // Close if already visible
+                    if (wasOpen) {
+                        return;
+                    }
+
+                    // Pass empty string as value to search for, displaying all results
+                    input.autocomplete("search", "");
+                });
+        },
+
+        _source: function (request, response) {
+            var matcher = new RegExp($.ui.autocomplete.escapeRegex(request.term), "i");
+            response(this.element.children("option").map(function () {
+                var text = $(this).text();
+                if (this.value && (!request.term || matcher.test(text)))
+                    return {
+                        label: text,
+                        value: text,
+                        option: this
+                    };
+            }));
+        },
+
+        _removeIfInvalid: function (event, ui) {
+
+            // Selected an item, nothing to do
+            if (ui.item) {
+                return;
+            }
+
+            // Search for a match (case-insensitive)
+            var value = this.input.val(),
+                valueLowerCase = value.toLowerCase(),
+                valid = false;
+            this.element.children("option").each(function () {
+                if ($(this).text().toLowerCase() === valueLowerCase) {
+                    this.selected = valid = true;
+                    return false;
+                }
+            });
+
+            // Found a match, nothing to do
+            if (valid) {
+                return;
+            }
+
+            // Remove invalid value
+            this.input
+                .val("")
+                .attr("title", value + "no hay coincidencias")
+                .tooltip("open");
+            this.element.val("");
+            this._delay(function () {
+                this.input.tooltip("close").attr("title", "");
+            }, 2500);
+            this.input.autocomplete("instance").term = "";
+        },
+
+        _destroy: function () {
+            this.wrapper.remove();
+            this.element.show();
+        }
+    });
+
+    $("#IdCliente").combobox();
+    $("#toggle").on("click", function () {
+        $("#IdCliente").toggle();
+    });
+    
 
 }
