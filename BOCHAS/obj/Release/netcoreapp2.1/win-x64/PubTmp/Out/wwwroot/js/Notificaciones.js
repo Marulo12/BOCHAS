87a80347c496a74ee785caef1b7d5b6a29212a9e
mario boscatto <mariolucbos@hotmail.com>
2018-08-16 10:09:42 -0300

    Arreglos

---
+++ BOCHAS/obj/Release/netcoreapp2.1/win-x64/PubTmp/Out/wwwroot/js/Notificaciones.js
@@ -0,0 +1,83 @@
+﻿
+
+//var connection1 = $.hubConnection(), hub = connection1.createHubProxy('chat');
+var user = function (user) {
+    if (user.length === 0 ) {
+        $("#Sig").empty();
+        $("#Sig").append('<li><a asp-action="Index" asp-controller="Sessions" class="notification-item"><span class="dot bg-danger"></span> No hay jugadores  Conectados</a></li>');
+    }
+    else {
+        $("#Sig").empty();
+        for (var i = 0; i < user.length; i++) {
+
+            $("#Sig").append('<li><a asp-action="Index" asp-controller="Sessions" class="notification-item"><span class="dot bg-success"></span>' + user[i].us + ' Conectado</a></li>');
+        }
+    }   
+}
+
+var reservaJ = function (reservaJ) {
+    $("#Listareserva").empty();
+    if (reservaJ.length === 0) {
+        $("#Lreserva").html("0");
+        $("#Listareserva").html('<li><a asp-action="Index" asp-controller="Sessions" class="notification-item"><span class="dot bg-danger"></span> No hay reservas Confirmadas en el dia de hoy</a></li>');
+    }
+    else {
+        $("#Lreserva").html(reservaJ.length);
+        for (var i = 0; i < reservaJ.length; i++) {
+
+            $("#Listareserva").append('<li><a asp-action="Index" asp-controller="Sessions" class="notification-item"><span class="dot bg-success"></span> Reserva Confirmada N°' + reservaJ[i].Numero + ', por ' + reservaJ[i].Nombre + " " + reservaJ[i].Apellido + '</a></li>');
+        }
+
+    }
+}
+
+
+hub.on('join', user);
+//hub.on('ReservasJugador', reservaJ);
+//connection1.start(
+  //  function () {
+      //  
+     //   hub.invoke('ReservasJugador');
+   // }).done();
+
+
+$.ajax({
+    type: "GET",
+    url: "/Sessions/MostrarTotalSesiones",    
+    success: function (response) {
+
+        $("#TotalSesiones").text(response);
+    }
+
+});
+
+$.ajax({
+    type: "GET",
+    url: "/AlquilerCanchas/TraerReservasPorDia",
+    success: function (response) {
+
+        $("#BodyTReservaPorDia").empty();
+        var label = "";
+        for (var i = 0; i < response.length; i++)
+        {
+            
+            switch (response[i].ide) {
+                case 2:
+                    label = "label-warning";
+                    break;
+                case  3:
+                    label = "label-info";
+                    break;
+                case 4:
+                    label = "label-success";
+                    break;
+                case 5:
+                    label = "label-danger";
+                    break;
+            }
+
+            $("#BodyTReservaPorDia").append('<tr><td>' + response[i].numero + '</td><td>' + response[i].nombre + '</td><td><label class="label ' + label + ' " style="font-size:12px;">' + response[i].estado + '</label></td></tr>');
+        }
+    }
+
+});
---
+++ BOCHAS/obj/Release/netcoreapp2.1/win-x64/PubTmp/Out/wwwroot/js/Sessiones.js
@@ -0,0 +1,89 @@
+﻿$(document).ready(function () {
+
+    $("#BtnSessionMostrar").click(function () {
+        MostrarSessiones();
+    });
+
+});
+
+function MostrarSessiones() {
+
+    if (ComprobarCampos()) {
+        var fechadesde = $("#fechadesde").val();
+        var fechahasta = $("#fechahasta").val();
+
+        $("#DivSessiones").html('<img id="ImgLoad" src="../images/extras/dots-2.gif" alt="" width="200" />');
+
+        $.ajax({
+            type: "GET",
+            url: "/Sessions/MostrarSessiones",
+            data: { fechadesde, fechahasta },
+            success: function (response) {
+
+                $("#DivSessiones").html(response);
+                $("#TablaSessiones").DataTable({
+                    searching: true,                   
+                    lengthMenu: [5, 10, 20, 75, 100],
+                    responsive: true,
+                    search: "Filtro&nbsp;:",                   
+                    dom: 'Bfrtip',
+                    buttons: [
+                        {
+                            extend: 'excel',
+                            text: 'Excel',
+                            title: 'BOCHAS PADEL - Sesiones'
+                        },
+                        {
+                            extend: 'pdfHtml5',
+                            text: 'PDF',
+                            title: 'BOCHAS PADEL - Sesiones'
+
+                        },
+                        {
+                            extend: 'print',
+                            text: 'Imprimir',
+                            title: 'BOCHAS PADEL - Sesiones'
+
+                        }
+                    ],
+                    language: {
+                        processing: "Procesando",
+                        search: "Filtro&nbsp;:",
+                        info: "Pagina _PAGE_ de _PAGES_  / <b>Total de Registros: _MAX_</b> ",
+                        infoEmpty: "",
+                        infoFiltered: "",
+                        zeroRecords: "Ningun registro coincide",
+                        lengthMenu: "Mostrar _MENU_ registros",
+                        infoPostFix: "",
+                        loadingRecords: "Cargando...",
+                        emptyTable: "No hay registros",
+                        paginate: {
+                            first: "Primero",
+                            previous: "Anterior",
+                            next: "Siguiente",
+                            last: "Ultimo"
+                        }
+                    }
+                });
+                $("#TablaSessiones_filter").append($("div .btn-group"));
+                
+                $("div .btn-group").css("float", "left");
+            }
+        });
+    }
+}
+
+function ComprobarCampos() {
+    if ($("#fechadesde").val() === "") {
+
+        alertify.error('No cargo la fecha desde');
+        return false;
+    }
+    if ($("#fechahasta").val() === "") {
+        alertify.error('No cargo la fecha hasta');
+        return false;
+    }
+   
+
+    return true;
+}
