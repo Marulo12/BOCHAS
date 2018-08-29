32add78bdb9137c50a532f047c023a38f9161a3a
mario boscatto <mariolucbos@hotmail.com>
2018-08-29 13:30:28 -0300

    Interfaz Registrar Clase particular

---
+++ BOCHAS/Controllers/ClaseParticularsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using BOCHAS.Models;
+
+namespace BOCHAS.Controllers
+{
+    public class ClaseParticularsController : Controller
+    {
+        private readonly BOCHASContext _context;
+
+        public ClaseParticularsController(BOCHASContext context)
+        {
+            _context = context;
+        }
+
+        // GET: ClaseParticulars
+        public  IActionResult Index()
+        {
+            ViewData["IdJugador"] = new SelectList(_context.Persona.Where(p=>p.Tipo =="JUGADOR" && p.FechaBaja == null &&  _context.Jugador.Where(j=>j.IdPersona == p.Id && j.IdTipoJugador == 2).Count() > 0).Select( x => new { Id = x.Id , persona = x.Nombre + " " + x.Apellido + ", Nro doc: " + x.NroDocumento  }), "Id", "persona");
+            ViewData["IdProfesor"] = new SelectList(_context.Persona.Where(p=>p.FechaBaja == null && p.Tipo =="EMPLEADO").Select(x => new { Id = x.Id, persona = x.Nombre + " " + x.Apellido + ", Nro doc: " + x.NroDocumento }), "Id", "persona");
+            return View();
+        }
+
+    }
+}
---
+++ BOCHAS/wwwroot/js/ClasesParticulares.js
@@ -0,0 +1,97 @@
+﻿$(document).ready(function () {
+
+    $('[data-toggle="tooltip"]').tooltip();
+    $("#BtnValida").click(function () {
+
+        TraerCanchas();
+    });
+    $("#BtnOcupadas").click(function () {
+
+        MostrarHorariosOcupados();
+    });
+    
+    
+    
+
+});
+
+
+
+function MostrarHorariosOcupados() {
+    var fecR = $("#FecR").val();
+    if (fecR !== "") {
+        $("#ModalHorarios").modal();
+        $.ajax({
+            type: "GET",
+            data: { fecR },
+            url: "/Agenda/MostrarHorariosOcupados",
+            success: function (response) {
+                $("#ModalHorariosBody").html(response);
+                $("#TablaHorariosOcupados").DataTable({
+                    searching: true,
+                    pageLength: 50,
+                    responsive: true,
+                    search: "Filtro&nbsp;:",
+                    language: {
+                        processing: "Procesando",
+                        search: "Filtro&nbsp;:",
+                        info: "Pagina _PAGE_ de _PAGES_  / <b>Total de Registros: _MAX_</b> ",
+                        infoEmpty: "",
+                        infoFiltered: "",
+                        zeroRecords: "Ningun registro coincide",
+                        lengthMenu: "Mostrar _MENU_ registros",
+                        infoPostFix: "",
+                        loadingRecords: "Cargando...",
+                        emptyTable: "No hay registros",
+                        paginate: {
+                            first: "Primero",
+                            previous: "Anterior",
+                            next: "Siguiente",
+                            last: "Ultimo"
+                        }
+                    }
+                });
+               
+
+            },
+            failure: function (response) {
+                alert(response);
+            }
+
+        });
+    } else { alertify.error("Para consultar horarios tiene que incorporar una fecha de reserva"); }
+}
+
+function ComprobarCamposDates() {
+
+    if ($("#FecR").val() === "") {
+        alertify.error("Complete el campo fecha de Reserva");
+        return false;
+    }
+    if ($("#FecR").val() < $("#FecP").val()) {
+        alertify.error("La fecha de reserva no puede ser menor que la fecha ACTUAL");
+        return false;
+    }
+
+    if ($("#HD").val() === "") {
+        alertify.error("Complete el campo Hora Desde");
+        return false;
+    }
+    if ($("#HH").val() === "") {
+        alertify.error("Complete el campo Hora Hasta");
+        return false;
+    }
+
+    if ($("#HH").val() <= $("#HD").val()) {
+        alertify.error("La Hora hasta no puede ser menor o igual que la hora desde");
+        return false;
+    }
+
+    if ($("#FecR").val() === $("#FecP").val()) {
+        if ($("#HD").val() < $("#HR").val()) {
+            alertify.error("La hora desde no puede ser menor a la hora actual");
+            return false;
+        }
+    }
+    return true;
+}
